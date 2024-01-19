20d5af10281e82d1af3429bdbe172ba7cddc5ed9
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2024-01-19 15:25:33 +0400

    add custom font, colors and extension

--- Recipes App/Scenes/LandingPage/LandingPageViewController.swift
+++ Recipes App/Scenes/LandingPage/LandingPageViewController.swift
@@ -7,10 +7,90 @@
 
 import UIKit
 
-class LandingPageViewController: UIViewController {
+final class LandingPageViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    private let greetingLabel = {
+        let label = UILabel()
+        label.text = "დილა მშვიდობისა,\nსესილი"
+        label.font = FontManager.shared.bodyFont
+        label.numberOfLines = 0
+        label.textColor = ColorManager.shared.textGrayColor
+        return label
+    }()
+    
+    private let whatAreYouCookingLabel = {
+        let label = UILabel()
+        label.text = "დღეს რას მოამზადებ?" .uppercased()
+        label.font = FontManager.shared.headlineFont
+        return label
+    }()
+    
+    private let labelStack = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 18
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private let categoriesLabel = {
+        let label = UILabel()
+        label.text = "კატეგორიები" .uppercased()
+        label.font = FontManager.shared.headlineFont
+        return label
+    }()
+    
+    private let recommendationsLabel = {
+        let label = UILabel()
+        label.text = "რეკომენდაციები" .uppercased()
+        label.font = FontManager.shared.headlineFont
+        return label
+    }()
+    
+    private let mainStack = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 58
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    //MARK: - ViewLifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = ColorManager.shared.backgroundColor
+        
+        addViews()
+        addConstraints()
     }
+    
+    //MARK: - Add Views
+    
+    private func addViews() {
+        labelStack.addArrangedSubview(greetingLabel)
+        labelStack.addArrangedSubview(whatAreYouCookingLabel)
+        
+        mainStack.addArrangedSubview(labelStack)
+        mainStack.addArrangedSubview(categoriesLabel)
+        mainStack.addArrangedSubview(recommendationsLabel)
+        
+        view.addSubview(mainStack)
+
+    }
+    
+    //MARK: - Add Constraints
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            mainStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            mainStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 35)
+        ])
+    }
+
 }
 
