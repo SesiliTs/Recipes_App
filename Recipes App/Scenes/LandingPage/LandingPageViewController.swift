9d2514e80fb05f95ceb4615a62cb298d123fd0d1
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2024-01-19 13:27:36 +0400

    add package dependencies

---
+++ Recipes App/Scenes/LandingPage/LandingPageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Recipes App
+//
+//  Created by Sesili Tsikaridze on 19.01.24.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
