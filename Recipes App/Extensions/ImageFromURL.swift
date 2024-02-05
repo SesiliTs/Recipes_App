5c40d0261352971bdbc3dc6d4a5838eb62c4271d
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2024-02-05 19:24:18 +0400

    change image compression

--- Recipes App/Extensions/ImageFromURL.swift
+++ Recipes App/Extensions/ImageFromURL.swift
@@ -10,10 +10,16 @@ import SwiftUI
 
 extension UIImageView {
     func load(urlString: String) {
+        let compressionQuality: CGFloat = 0.1
+        
         if let url = URL(string: urlString) {
             DispatchQueue.global().async { [weak self] in
                 if let data = try? Data(contentsOf: url) {
-                    if let image = UIImage(data: data) {
+                    if var image = UIImage(data: data) {
+                        if let compressedData = image.jpegData(compressionQuality: compressionQuality) {
+                            image = UIImage(data: compressedData) ?? image
+                        }
+                        
                         DispatchQueue.main.async {
                             self?.image = image
                         }
