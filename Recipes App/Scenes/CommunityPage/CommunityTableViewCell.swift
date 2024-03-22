d18ffea33da3fcf69da448a8c5d9a1f09f835a3a
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2024-03-23 00:13:48 +0400

    make UI for questions list

--- Recipes App/Scenes/CommunityPage/CommunityTableViewCell.swift
+++ Recipes App/Scenes/CommunityPage/CommunityTableViewCell.swift
@@ -20,12 +20,87 @@ class CommunityTableViewCell: UITableViewCell {
         return view
     }()
     
+    let circleContainer = {
+        let view = UIView()
+        view.backgroundColor = .systemPink
+        view.layer.cornerRadius = 20
+        view.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        view.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        view.layer.masksToBounds = true
+        return view
+    }()
+
+    let contentImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     private let userNameLabel = {
         let label = UILabel()
         label.text = "სესილი წიქარიძე"
         label.font = FontManager.shared.bodyFont
         label.textColor = ColorManager.shared.textGrayColor
         return label
+    }()
+    
+    private let dateLabel = {
+        let label = UILabel()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy, HH:mm"
+        label.text = dateFormatter.string(from: Date())
+        label.font = FontManager.shared.bodyFont
+        label.textColor = ColorManager.shared.textGrayColor
+        return label
+
+    }()
+    
+    private lazy var nameStack = {
+        let stackView = UIStackView (arrangedSubviews: [userNameLabel, dateLabel])
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    private lazy var imageStack = {
+        circleContainer.addSubview(contentImageView)
+        let stackView = UIStackView (arrangedSubviews: [circleContainer, nameStack])
+        stackView.spacing = 10
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let commentsIcon = {
+        let imageView = UIImageView()
+        imageView.image = .init(systemName: "bubble.right")
+        imageView.tintColor = ColorManager.shared.textLightGray
+        imageView.widthAnchor.constraint(equalToConstant: 16).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 16).isActive = true
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let commentsLabel = {
+        let label = UILabel()
+        label.text = "12"
+        label.textColor = ColorManager.shared.textLightGray
+        label.font = FontManager.shared.bodyFont
+        return label
+    }()
+    
+    private lazy var commentsStack = {
+        let stackView = UIStackView (arrangedSubviews: [commentsIcon, commentsLabel])
+        stackView.spacing = 5
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private lazy var detailsStack = {
+        let stackView = UIStackView (arrangedSubviews: [imageStack, commentsStack])
+        stackView.spacing = 10
+        stackView.alignment = .top
+        stackView.distribution = .equalCentering
+        return stackView
     }()
     
     private let questionLabel = {
@@ -37,9 +112,9 @@ class CommunityTableViewCell: UITableViewCell {
     }()
     
     private lazy var mainStack = {
-        let stackView = UIStackView (arrangedSubviews: [questionLabel, userNameLabel])
+        let stackView = UIStackView (arrangedSubviews: [detailsStack, questionLabel])
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.spacing = 15
+        stackView.spacing = 10
         stackView.axis = .vertical
         return stackView
     }()
